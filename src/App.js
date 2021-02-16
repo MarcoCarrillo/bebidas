ffcb55929c8c03d8c489d0a3b5cbb942a100c8ae
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-02-16 12:30:13 -0600

    creando el segundo context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, {Fragment}from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
+
 import CategoriasProvider from './context/CategoriasContext';
+import RecetasProvider from './context/RecetasContext';
+
 
 function App() {
   return (
     <CategoriasProvider>
+      <RecetasProvider>
       <Header />
 
       <div className="container mt-5">
@@ -13,6 +17,7 @@ function App() {
           <Formulario />
         </div>
       </div>
+      </RecetasProvider>
     </CategoriasProvider>
    
   );
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, {useContext, useState} from 'react';
 import {CategoriasContext} from '../context/CategoriasContext'
+import {RecetasContext} from '../context/RecetasContext'
 
 const Formulario = () => {
 
@@ -8,6 +9,7 @@ const Formulario = () => {
         categoria: ''
     })
     const { categorias } = useContext(CategoriasContext);
+    const { buscarRecetas } = useContext(RecetasContext)
     
     //funcion para obtener los contenidos
     const obtenerDatosReceta = e => {
@@ -18,7 +20,13 @@ const Formulario = () => {
     }
 
     return ( 
-        <form className="col-12">
+        <form 
+            className="col-12"
+            onSubmit={e => {
+                e.preventDefault();
+                buscarRecetas(busqueda)
+            }}
+        >
             <fieldset className="text-center">
                 <legend>Busca bebidas por Categoría o Ingrediente</legend>
             </fieldset>
